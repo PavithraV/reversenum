fd5ec22f6d15d3dec68c0530062047aa1ded4869
PavithraV <pavithradeepa96@gmail.com>
2016-01-20 23:27:26 +0530

    reversenum.c

---
+++ reversenum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int reverse(int n)
+{
+		int rev=0;
+		printf("enter the number to reverse\n");
+		scanf("%d",&n);
+		while(n>0)
+		{
+			rev=rev * 10;
+			rev=rev + (n%10);
+			n=n/10;
+		}
+		return rev;
+}
+int main()
+{
+	    int n;
+	    printf("enter the number to reverse\n");
+		scanf("%d",&n);
+		printf("reversed num=%d",reverse(n));
+		return 0;
